fd909adde0f0e71f8f1845bff1131fd952ba0f4c
俱欢 <juhuan.wy@antfin.com>
2020-08-31 15:32:37 +0800

    feat: filter field names

--- src/main/java/ink/organics/pojo2json/POJO2JsonAction.java
+++ src/main/java/ink/organics/pojo2json/POJO2JsonAction.java
@@ -24,17 +24,28 @@ import java.util.List;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * The type Pojo 2 json action.
+ *
+ * @author juhuan.wy
+ */
 public class POJO2JsonAction extends AnAction {
-
-
-    private static final NotificationGroup notificationGroup =
-            new NotificationGroup("pojo2json.NotificationGroup", NotificationDisplayType.BALLOON, true);
-
+    /**
+     * The constant notificationGroup.
+     */
+    private static final NotificationGroup notificationGroup = new NotificationGroup("pojo2json.NotificationGroup", NotificationDisplayType.BALLOON, true);
+    /**
+     * The constant normalTypes.
+     */
     @NonNls
     private static final Map<String, Object> normalTypes = new HashMap<>();
-
+    /**
+     * The constant gsonBuilder.
+     */
     private static final GsonBuilder gsonBuilder = new GsonBuilder().setPrettyPrinting();
-
+    /**
+     * The constant zero.
+     */
     private static final BigDecimal zero = BigDecimal.ZERO.setScale(2, RoundingMode.UNNECESSARY);
 
     static {
@@ -56,6 +67,23 @@ public class POJO2JsonAction extends AnAction {
         normalTypes.put("LocalTime", now.toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
     }
 
+    /**
+     * The constant SKIP_FIELD_NAMES.
+     */
+    private static final List<String> SKIP_FIELD_NAMES = new ArrayList<String>() {{
+        add("serialVersionUID");
+    }};
+    /**
+     * The constant SKIP_FIELD_TYPES.
+     */
+    private static final List<String> SKIP_FIELD_TYPES = new ArrayList<String>() {{
+    }};
+
+    /**
+     * Action performed.
+     *
+     * @param e the e
+     */
     @Override
     public void actionPerformed(AnActionEvent e) {
         Editor editor = e.getData(CommonDataKeys.EDITOR);
@@ -83,7 +111,12 @@ public class POJO2JsonAction extends AnAction {
         }
     }
 
-
+    /**
+     * 获取所有字段
+     *
+     * @param psiClass the psi class
+     * @return the fields
+     */
     private static Map<String, Object> getFields(PsiClass psiClass) {
         Map<String, Object> map = new LinkedHashMap<>();
 
@@ -92,29 +125,48 @@ public class POJO2JsonAction extends AnAction {
         }
 
         for (PsiField field : psiClass.getAllFields()) {
-            map.put(field.getName(), typeResolve(field.getType(), 0));
+
+            String name = field.getName();
+            PsiType type = field.getType();
+
+            if (SKIP_FIELD_NAMES.contains(name)) {
+                continue;
+            }
+            if (SKIP_FIELD_TYPES.contains(type.toString())) {
+                continue;
+            }
+
+            map.put(name, typeResolve(type, 0));
         }
 
         return map;
     }
 
-
+    /**
+     * Type resolve object.
+     *
+     * @param type  the type
+     * @param level the level
+     * @return the object
+     */
     private static Object typeResolve(PsiType type, int level) {
 
         level = ++level;
-
-        if (type instanceof PsiPrimitiveType) {       //primitive Type
+        if (type instanceof PsiPrimitiveType) {
+            //array type
 
             return getDefaultValue(type);
 
-        } else if (type instanceof PsiArrayType) {   //array type
+        } else if (type instanceof PsiArrayType) {
+            //array type
 
             List<Object> list = new ArrayList<>();
             PsiType deepType = type.getDeepComponentType();
             list.add(typeResolve(deepType, level));
             return list;
 
-        } else {    //reference Type
+        } else {
+            //reference Type
 
             Map<String, Object> map = new LinkedHashMap<>();
 
@@ -123,8 +175,8 @@ public class POJO2JsonAction extends AnAction {
             if (psiClass == null) {
                 return map;
             }
-
-            if (psiClass.isEnum()) { // enum
+            // enum
+            if (psiClass.isEnum()) {
 
                 for (PsiField field : psiClass.getFields()) {
                     if (field instanceof PsiEnumConstant) {
@@ -142,14 +194,17 @@ public class POJO2JsonAction extends AnAction {
                 fieldTypeNames.add(type.getPresentableText());
                 fieldTypeNames.addAll(Arrays.stream(types).map(PsiType::getPresentableText).collect(Collectors.toList()));
 
-                if (fieldTypeNames.stream().anyMatch(s -> s.startsWith("Collection") || s.startsWith("Iterable"))) {// Iterable
+
+                if (fieldTypeNames.stream().anyMatch(s -> s.startsWith("Collection") || s.startsWith("Iterable"))) {
+                    // Iterable
 
                     List<Object> list = new ArrayList<>();
                     PsiType deepType = PsiUtil.extractIterableTypeParameter(type, false);
                     list.add(typeResolve(deepType, level));
                     return list;
 
-                } else { // Object
+                } else {
+                    // Object
 
                     List<String> retain = new ArrayList<>(fieldTypeNames);
                     retain.retainAll(normalTypes.keySet());
@@ -162,7 +217,15 @@ public class POJO2JsonAction extends AnAction {
                         }
 
                         for (PsiField field : psiClass.getAllFields()) {
-                            map.put(field.getName(), typeResolve(field.getType(), level));
+                            String name = field.getName();
+                            PsiType type1 = field.getType();
+                            if (SKIP_FIELD_NAMES.contains(name)) {
+                                continue;
+                            }
+                            if (SKIP_FIELD_TYPES.contains(type1.toString())) {
+                                continue;
+                            }
+                            map.put(name, typeResolve(type1, level));
                         }
 
                         return map;
@@ -172,24 +235,30 @@ public class POJO2JsonAction extends AnAction {
         }
     }
 
-
+    /**
+     * 获取该类型的默认值
+     *
+     * @param type the type
+     * @return the default value
+     */
     public static Object getDefaultValue(PsiType type) {
-        if (!(type instanceof PsiPrimitiveType)) return null;
+        if (!(type instanceof PsiPrimitiveType)) {
+            return null;
+        }
         switch (type.getCanonicalText()) {
             case "boolean":
                 return false;
             case "byte":
                 return (byte) 0;
-            case "char":
-                return '\0';
             case "short":
                 return (short) 0;
             case "int":
                 return 0;
             case "long":
                 return 0L;
+            case "char":
+                return '\0';
             case "float":
-                return zero;
             case "double":
                 return zero;
             default:
